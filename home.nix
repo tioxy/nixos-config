125a0be048b81b6575a39865e5898892ba896428
Gabriel Tiossi <gabrieltiossi@gmail.com>
2024-01-13 00:33:09 +0000

    Initial commit

---
+++ home.nix
@@ -0,0 +1,110 @@
+{ config, pkgs, lib, ... }:
+
+# let
+# kube_ps1 = builtins.fetchGit {
+#   name = "kube-ps1";
+#   url = "https://github.com/jonmosco/kube-ps1";
+#   ref = "refs/tags/v0.8.0";
+# };
+# in 
+{ 
+  programs.home-manager.enable = true;
+  
+  nixpkgs.config.allowUnfree = true;
+
+  home = {
+    username = "tioxy";
+    homeDirectory = "/home/tioxy";
+    stateVersion = "23.11";
+    packages = with pkgs; [
+      jq
+      bat
+      htop
+      watch
+      tree
+      wget
+      fzf
+      _1password
+
+      # nvim
+      ripgrep
+      fd
+      tree-sitter
+      nodejs
+      unzip
+      stylua
+      gcc
+
+      awscli2
+    ];
+    sessionVariables = {
+      LANG = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      EDITOR = "nvim";
+      PAGER = "less -FirSwX";
+    };
+  };
+
+  fonts.fontconfig.enable = true;
+
+  # Shell
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "home-manager switch";
+      k = "kubectl";
+    };
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "sudo"
+        "asdf"
+      ];
+      theme = "steeef";
+    };
+  };
+
+
+  xdg.configFile.nvim = {  
+    source = ./nvim;
+    recursive = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  programs.lazygit = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Gabriel Tiossi";
+    userEmail = "gabrieltiossi@gmail.com";
+    aliases = {
+      prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      root = "rev-parse --show-toplevel";
+    };
+    extraConfig = {
+      branch.autosetuprebase = "always";
+      color.ui = true;
+      core.askPass = "";
+      core.fileMode = false;
+      credential.helper = "store";
+      github.user = "tioxy";
+      push.default = "tracking";
+      init.defaultBranch = "main";
+      safe.directory = ["*"];
+    };
+  };
+}
---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,111 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+require("rose-pine").setup({
+  variant = "auto", -- auto, main, moon, or dawn
+  dark_variant = "main", -- main, moon, or dawn
+  dim_inactive_windows = false,
+  extend_background_behind_borders = true,
+
+  styles = {
+    bold = true,
+    italic = true,
+    transparency = false,
+  },
+
+  groups = {
+    border = "muted",
+    link = "iris",
+    panel = "surface",
+
+    error = "love",
+    hint = "iris",
+    info = "foam",
+    warn = "gold",
+
+    git_add = "foam",
+    git_change = "rose",
+    git_delete = "love",
+    git_dirty = "rose",
+    git_ignore = "muted",
+    git_merge = "iris",
+    git_rename = "pine",
+    git_stage = "iris",
+    git_text = "rose",
+    git_untracked = "subtle",
+
+    headings = {
+      h1 = "iris",
+      h2 = "foam",
+      h3 = "rose",
+      h4 = "gold",
+      h5 = "pine",
+      h6 = "foam",
+    },
+    -- Alternatively, set all headings at once.
+    -- headings = "subtle",
+  },
+
+  highlight_groups = {
+    -- Comment = { fg = "foam" },
+    -- VertSplit = { fg = "muted", bg = "muted" },
+  },
+
+  before_highlight = function(group, highlight, palette)
+    -- Disable all undercurls
+    -- if highlight.undercurl then
+    --     highlight.undercurl = false
+    -- end
+    --
+    -- Change palette colour
+    -- if highlight.fg == palette.pine then
+    --     highlight.fg = palette.foam
+    -- end
+  end,
+})
+
+vim.cmd("colorscheme rose-pine")
