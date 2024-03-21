789072401e3b46e27879ff315f7e6a45f7c56dcd
Gabriel Tiossi <gabrieltiossi@gmail.com>
2024-03-21 04:41:07 +0000

    feat: rose-pine nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -42,8 +42,8 @@ require("lazy").setup({
 })
 
 require("rose-pine").setup({
-  variant = "auto", -- auto, main, moon, or dawn
-  dark_variant = "main", -- main, moon, or dawn
+  variant = "dawn", -- auto, main, moon, or dawn
+  dark_variant = "dawn", -- main, moon, or dawn
   dim_inactive_windows = false,
   extend_background_behind_borders = true,
 
